975680af5b7429b4c5812296a53df35f684a4aa1
azedine ouhadou <xperazazedine@gmail.com>
2025-10-29 15:52:22 +0100

    feat: implement database schema and validation middleware for users and habits

--- src/routes/habitRoutes.ts
+++ src/routes/habitRoutes.ts
@@ -1,4 +1,14 @@
 import { Router } from 'express'
+import { validateBody, validateParams } from '../middleware/validation.ts'
+import { object, z } from 'zod'
+
+const createHabitSchema = z.object({
+  name: z.string(),
+})
+
+const completeParamsSchema = z.object({
+  id: z.string(),
+})
 
 const router = Router()
 
@@ -10,16 +20,21 @@ router.get('/:id', (req, res) => {
   res.json({ message: `get habit with id: ${req.params.id}` })
 })
 
-router.post('/', (req, res) => {
-  res.json({ message: 'create a habit' }).status(2001)
+router.post('/', validateBody(createHabitSchema), (req, res) => {
+  res.json({ message: 'create a habit' }).status(201)
 })
 
 router.delete('/:id', (req, res) => {
   res.json({ message: `delete habit with id: ${req.params.id}` }).status(202)
 })
 
-router.post('/:id', (req, res) => {
-  res.json({ message: `complete habit: ${req.params.id}` }).status(201)
-})
+router.post(
+  '/:id/complete',
+  validateParams(completeParamsSchema),
+  validateBody(createHabitSchema),
+  (req, res) => {
+    res.json({ message: `complete habit: ${req.params.id}` }).status(201)
+  }
+)
 
 export default router
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,22 @@ import express from 'express'
 import authRoutes from './routes/authRoutes.ts'
 import habitRoutes from './routes/habitRoutes.ts'
 import userRoutes from './routes/userRoutes.ts'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { skip } from 'node:test'
+import { isTest } from '../env.ts'
 
 const app = express()
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(
+  morgan('dev', {
+    skip: () => isTest(),
+  })
+)
 
 app.get('/health', (req, res) => {
   res.json({ message: 'Server is healthy' }).status(200)
