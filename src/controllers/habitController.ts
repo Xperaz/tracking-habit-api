3e96d4932760c49535cf512db091998bed4f08c2
azedine ouhadou <xperazazedine@gmail.com>
2025-11-13 22:39:12 +0100

    feat: implement habit management routes and controller functions

---
+++ src/controllers/habitController.ts
@@ -0,0 +1,115 @@
+import type { Response } from 'express'
+import type { AuthenticatedRequest } from '../middleware/auth.ts'
+import { db } from '../db/connection.ts'
+import { habits, entries, habitTags, tags } from '../db/schema.ts'
+import { and, eq, inArray, desc } from 'drizzle-orm'
+
+export const createHabit = async (req: AuthenticatedRequest, res: Response) => {
+  try {
+    const { name, description, frequency, targetCount, tagIds } = req.body
+
+    const result = await db.transaction(async (tx) => {
+      const [newHabit] = await tx
+        .insert(habits)
+        .values({
+          userId: req.user.id,
+          name,
+          description,
+          frequency,
+          targetCount,
+        })
+        .returning()
+
+      if (tagIds && tagIds.length > 0) {
+        const habitTagValues = tagIds.map((tagId: number) => ({
+          habitId: newHabit.id,
+          tagId,
+        }))
+
+        await tx.insert(habitTags).values(habitTagValues)
+      }
+
+      return newHabit
+    })
+    res.status(201).json({
+      message: 'Habit created successfully',
+      habit: result,
+    })
+  } catch (error) {
+    console.error('Error creating habit:', error)
+    res.status(500).json({ message: 'Failed to create habit' })
+  }
+}
+
+export const getUserHabits = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  try {
+    const userHabitsWithTags = await db.query.habits.findMany({
+      where: eq(habits.userId, req.user.id),
+      with: {
+        habitTags: {
+          with: {
+            tag: true,
+          },
+        },
+      },
+      orderBy: [desc(habits.createdAt)],
+    })
+
+    const habitsWithTags = userHabitsWithTags.map((habit) => ({
+      ...habit,
+      tags: habit.habitTags.map((ht) => ht.tag),
+      habitTags: undefined,
+    }))
+
+    res.status(200).json({
+      message: 'User habits retrieved successfully',
+      habits: habitsWithTags,
+    })
+  } catch (error) {
+    console.error('get habits error: ', error)
+    res.status(500).json({ message: 'Failed to get habits' })
+  }
+}
+
+export const updateHabit = async (req: AuthenticatedRequest, res: Response) => {
+  try {
+    const id = req.params.id
+    const { tagIds, ...updates } = req.body
+
+    const result = await db.transaction(async (tx) => {
+      const [updatedHabit] = await tx
+        .update(habits)
+        .set({ ...updates, updateAt: new Date() })
+        .where(and(eq(habitTags.id, id), eq(habits.userId, req.user.id)))
+        .returning()
+      if (!updateHabit) {
+        return res.status(401).end()
+      }
+
+      if (tagIds) {
+        await tx.delete(habitTags).where(eq(habitTags.habitId, id))
+
+        if (tagIds.length > 0) {
+          const habitTagValues = tagIds.map((tagId: number) => ({
+            habitId: id,
+            tagId,
+          }))
+          await tx.insert(habitTags).values(habitTagValues)
+        }
+      }
+
+      return updatedHabit
+    })
+
+    res.status(200).json({
+      message: 'Habit updated successfully',
+      habit: result,
+    })
+  } catch (error) {
+    console.error('update habit error: ', error)
+    res.status(500).json({ message: 'Failed to update habit' })
+  }
+}
--- src/routes/habitRoutes.ts
+++ src/routes/habitRoutes.ts
@@ -2,9 +2,26 @@ import { Router } from 'express'
 import { validateBody, validateParams } from '../middleware/validation.ts'
 import { object, z } from 'zod'
 import { authenticateToken } from '../middleware/auth.ts'
+import {
+  createHabit,
+  getUserHabits,
+  updateHabit,
+} from '../controllers/habitController.ts'
 
 const createHabitSchema = z.object({
   name: z.string(),
+  description: z.string().optional(),
+  frequency: z.string(),
+  targetCount: z.number().optional(),
+  tagIds: z.array(z.string()).optional(),
+})
+
+const updateHabitSchema = z.object({
+  name: z.string().optional(),
+  description: z.string().optional(),
+  frequency: z.string().optional(),
+  targetCount: z.number().optional(),
+  tagIds: z.array(z.string()).optional(),
 })
 
 const completeParamsSchema = z.object({
@@ -15,17 +32,14 @@ const router = Router()
 
 router.use(authenticateToken)
 
-router.get('/', (req, res) => {
-  res.json({ message: 'get all habits' })
-})
+router.get('/', getUserHabits)
 
 router.get('/:id', (req, res) => {
   res.json({ message: `get habit with id: ${req.params.id}` })
 })
 
-router.post('/', validateBody(createHabitSchema), (req, res) => {
-  res.json({ message: 'create a habit' }).status(201)
-})
+router.post('/', validateBody(createHabitSchema), createHabit)
+router.patch('/:id', validateBody(updateHabitSchema), updateHabit)
 
 router.delete('/:id', (req, res) => {
   res.json({ message: `delete habit with id: ${req.params.id}` }).status(202)
