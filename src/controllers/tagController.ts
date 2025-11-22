384b8312ba01fe5021abc38913268092d115262a
azedine ouhadou <xperazazedine@gmail.com>
2025-11-22 12:05:51 +0100

    feat: add update and delete functionality for tags with validation

--- src/controllers/tagController.ts
+++ src/controllers/tagController.ts
@@ -93,3 +93,64 @@ export const getAllTags = async (req: AuthenticatedRequest, res: Response) => {
     res.status(500).json({ message: 'Failed to get tags' })
   }
 }
+
+export const updateTag = async (req: AuthenticatedRequest, res: Response) => {
+  try {
+    const { name, color } = req.body
+    const tagId = req.params.id
+
+    if (name) {
+      const existingTag = await db.query.tags.findFirst({
+        where: eq(tags.name, name),
+      })
+
+      if (existingTag) {
+        return res
+          .status(409)
+          .json({ message: 'Tag with this name already exist' })
+      }
+    }
+
+    const updatedTag = await db
+      .update(tags)
+      .set({
+        name: name || tags.name,
+        color: color || tags.color,
+        updatedAt: new Date(),
+      })
+      .where(eq(tags.id, tagId))
+      .returning()
+
+    if (!updatedTag) {
+      return res.status(404).json({ error: 'Tag not found' })
+    }
+
+    res.status(201).json({
+      message: 'Tag updated successfully',
+      tag: updatedTag,
+    })
+  } catch (error) {
+    console.error('error updating tag: ', error)
+    res.status(500).json({ message: 'Failed to update tag' })
+  }
+}
+
+export const deleteTag = async (req: AuthenticatedRequest, res: Response) => {
+  try {
+    const tagId = req.params.id
+
+    const [deletedTag] = await db
+      .delete(tags)
+      .where(eq(tags.id, tagId))
+      .returning()
+
+    if (!deletedTag) {
+      return res.status(404).json({ message: 'Tag not found!' })
+    }
+
+    res.status(200).json({ message: 'Tag was deleted successfully' })
+  } catch (error) {
+    console.error('Error while deleting: ', error)
+    res.status(500).json({ message: 'Failed to delete' })
+  }
+}
--- src/routes/tagRoutes.ts
+++ src/routes/tagRoutes.ts
@@ -2,15 +2,28 @@ import { Router } from 'express'
 import { authenticateToken } from '../middleware/auth.ts'
 import {
   createTag,
+  deleteTag,
   getAllTags,
   getTagById,
+  updateTag,
 } from '../controllers/tagController.ts'
 import z from 'zod'
 import { validateBody, validateParams } from '../middleware/validation.ts'
 
 const createTagSchema = z.object({
   name: z.string(),
-  color: z.string().optional,
+  color: z
+    .string()
+    .regex(/^#[0-9A-Fa-f]{6}$/, 'Color must be a valid hex color')
+    .optional(),
+})
+
+const updateTagSchema = z.object({
+  name: z.string().optional(),
+  color: z
+    .string()
+    .regex(/^#[0-9A-Fa-f]{6}$/, 'Color must be a valid hex color')
+    .optional(),
 })
 
 const uuidSchema = z.object({
@@ -26,5 +39,14 @@ router.post('/', validateBody(createTagSchema), createTag)
 router.get('/:id', validateParams(uuidSchema), getTagById)
 
 router.get('/', getAllTags)
+
+router.put(
+  '/:id',
+  validateBody(updateTagSchema),
+  validateParams(uuidSchema),
+  updateTag
+)
+
+router.delete('/:id', validateParams(uuidSchema), deleteTag)
 
 export default router
